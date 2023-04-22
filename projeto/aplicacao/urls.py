ae616f1debe973e451c6c294a6eff0af1989b2c0
hiago <joao.hiago.c.barros.com>
2023-04-22 00:38:09 -0300

    commit 10

--- projeto/aplicacao/urls.py
+++ projeto/aplicacao/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name="index"),
     path("<int:aplicacao_id>", views.aplicacao, name="aplicacao"),
-    
+    path('cadastro', views.cadastro, name='cadastro'),
+    path('logar', views.logar, name='logar'),
+    path('pagina', views.pagina, name='pagina'),
+    path('inicio', views.inicio, name='inicio')
 ]
--- projeto/aplicacao/views.py
+++ projeto/aplicacao/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
 from .models import Aplicacao, Lista
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 
+
+def inicio(request):
+    return render(request, 'aplicacao/inicio.html')
 
 def index(request):
     return render(request, "aplicacao/index.html", {
         "aplicacao": Aplicacao.objects.all()
     })
+
+
 
 
 def aplicacao(request, aplicacao_id):
@@ -16,3 +26,45 @@ def aplicacao(request, aplicacao_id):
 
       
     })
+
+
+
+
+def cadastro(request):
+    if request.method == 'GET':
+        return render(request, 'aplicacao/cadastro.html')
+    
+    else:
+        nome = request.POST.get('nome')
+        senha = request.POST.get('senha')
+
+        usuario = User.objects.filter(username=nome).first()
+        if usuario:
+            return HttpResponse(f'Usuário ja existe {nome}')
+        
+        usuario = User.objects.create_user(username=nome, password=senha)
+        usuario.save()
+        return HttpResponse(f'Usuário {nome} salvo com sucesso')
+    
+
+def logar(request):
+    if request.method == 'GET':
+        return render(request, 'aplicacao/logar.html')
+    
+    else:
+        nome = request.POST.get('nome')
+        senha = request.POST.get('senha')
+        usuario = authenticate(username=nome, password=senha)
+
+        if usuario:
+            login(request, usuario)
+            return HttpResponse('Usuario autenticado')
+        
+
+        else:
+            return HttpResponse('Login ou Senha invalidos')
+        
+
+@login_required(login_url='/logar')
+def pagina(request):
+    return HttpResponse('Página...')
