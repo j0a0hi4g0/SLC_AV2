f7b340932d3db5b787486d9b9b8b8830bf338d99
hiago <joao.hiago.c.barros.com>
2023-04-22 01:38:46 -0300

    commit 12

--- projeto/aplicacao/urls.py
+++ projeto/aplicacao/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name="index"),
     path("<int:aplicacao_id>", views.aplicacao, name="aplicacao"),
+    path('inicio', views.inicio, name='inicio'),
     path('cadastro', views.cadastro, name='cadastro'),
     path('logar', views.logar, name='logar'),
     path('pagina', views.pagina, name='pagina'),
-    path('inicio', views.inicio, name='inicio')
 ]
